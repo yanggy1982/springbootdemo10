457d72db03a73c1bbf14acb104e11314a6c61f58
yanggy1982 <yangemai0451@sina.com>
2020-04-26 06:45:04 +0800

    代码提交测试

---
+++ src/main/java/com/asiainfo/springbootdemo10/controller/LoginController.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2015-2020, 亚信科技（中国）有限公司
+ * Program     : springbootdemo10
+ * FileName    : LoginController.java
+ * Author      : yanggy
+ * Date        : 2020-04-26 06:43
+ * Description : 登陆控制器
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.asiainfo.springbootdemo10.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 登陆控制器
+ *
+ * @author Administrator
+ * @since 1.0.0
+ */
+@Controller
+public class LoginController {
+
+}
